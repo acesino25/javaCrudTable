225f0f6a0f9a474cf7e865f1d6eacf81043c3940
acesino25 <acesino25@hotmail.com>
2023-06-12 21:47:51 -0300

    origen

---
+++ app/src/main/java/crudTabla/logica/Auto.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crudTabla.logica;
+
+/**
+ *
+ * @author PC
+ */
+public class Auto {
+    private String id;
+    private String marca;
+    private String placa;
+
+    public Auto(String id, String marca, String placa) {
+        this.id = id;
+        this.marca = marca;
+        this.placa = placa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+    
+}
